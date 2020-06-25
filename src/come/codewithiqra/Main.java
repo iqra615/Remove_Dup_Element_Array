963b334c633b51288069d04eea9f23b8d36925b5
HP 6th <iqra615>
2020-06-24 18:10:13 -0700

    Remv_dup_Element_Array

---
+++ src/come/codewithiqra/Main.java
@@ -0,0 +1,25 @@
+package come.codewithiqra;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] a = {1, 2, 2, 3, 4, 6, 6};
+        int[] temp = new int[a.length];
+        int j = 0;
+
+        for (int i = 0; i < a.length - 1; i++) {
+            if (a[i] != a[i + 1]) {
+                temp[j] = a[i];
+                j++;
+            }
+        }
+        temp[j] = a[a.length - 1];
+        for (int i = 0; i < temp.length; i++)
+        {
+            System.out.println(temp[i]);
+        }
+    }
+}
